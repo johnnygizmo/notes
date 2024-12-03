c574f9d892fbdeafb0c374fd6e66846734e5e5e0
Johnny Matthews <johnny.matthews@gmail.com>
2024-12-02 19:30:01 -0600

    new root

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2275768228"
+    serviceWorkerVersion: "3438188665"
   }
 });
